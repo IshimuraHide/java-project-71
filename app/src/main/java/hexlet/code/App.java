e9b1d9e6e0870e82819da792e472519524433c1e
IshimuraHide <ktotodrugoyg@gmail.com>
2024-10-26 14:59:21 +0300

    Changed App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,22 +2,25 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
+
 
 //описание команды для picocli
-@Command(name = "gendiff", version = "App 1.0", mixinStandardHelpOptions = true)
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 
 
 public class App implements Runnable{
 
+    /*Опции в нашем случае не используются
     @Option(names = { "-s", "--font-size" }, description = "Font size")
     int fontSize = 19;
+     */
 
+    /*Параметры в нашем случае не используются
     @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
             description = "Words to be translated into ASCII art.")
     private String[] words = { "Hello,", "picocli" };
-
+     */
     @Override
     public void run() {
         // The business logic of the command goes here...
