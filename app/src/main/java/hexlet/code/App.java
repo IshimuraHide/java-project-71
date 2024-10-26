c8a757f46849ccb95af732e0b32b98481f84802d
IshimuraHide <ktotodrugoyg@gmail.com>
2024-10-26 14:40:28 +0300

    Changed logic of work run-dist

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,35 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+//описание команды для picocli
+@Command(name = "gendiff", version = "App 1.0", mixinStandardHelpOptions = true)
+
+
+public class App implements Runnable{
+
+    @Option(names = { "-s", "--font-size" }, description = "Font size")
+    int fontSize = 19;
+
+    @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
+            description = "Words to be translated into ASCII art.")
+    private String[] words = { "Hello,", "picocli" };
+
+    @Override
+    public void run() {
+        // The business logic of the command goes here...
+        // In this case, code for generation of ASCII art graphics
+        // (omitted for the sake of brevity).
     }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.out.println("Hello world!");
+        System.exit(exitCode);
+
+    }
+
+
 }
