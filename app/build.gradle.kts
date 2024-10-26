3570e7e687c5c93a49ed192b99f7fa987cbcc5bd
IshimuraHide <ktotodrugoyg@gmail.com>
2024-10-26 14:42:29 +0300

    Changing permission of gradlew

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("info.picocli:picocli:4.7.6")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
